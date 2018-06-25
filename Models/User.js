0d21bf18e53a9d2d3d380eda6fa4424e1721504d
je-ni <jenniferolibie@gmail.com>
2018-06-25 16:03:00 +0100

    added the relationship btw user, comment and post schemas

--- Models/User.js
+++ Models/User.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var UserSchema = mongoose.Schema({
     name: String,
     email: {type: String, unique: true},
-    password: String
+    password: String,
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
 })
 
 module.exports = mongoose.model('User', UserSchema);
