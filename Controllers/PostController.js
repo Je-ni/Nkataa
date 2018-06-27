465515d185f1ab1e34fefc459ac6b780f2a56fc5
je-ni <jenniferolibie@gmail.com>
2018-06-27 18:36:48 +0100

    modified addPost

---
+++ Controllers/PostController.js
@@ -0,0 +1,30 @@
+var service = require('../Services/PostService');
+
+exports.addPost = function(req, res){
+    var data = {
+        user: req.body.user,
+        time: Date.now(),
+        postBody: req.body.postBody
+    };
+    return service.addPost(req, res, data);
+}
+
+exports.getPosts = function(req, res){
+    return service.getAllPosts(req, res);
+}
+
+exports.deletePost = function(req, res){
+    var option = {_id: req.params.id};
+    return service.deletePost(req, res, option);
+}
+
+exports.getPostByParam = function(req, res){
+    var option = req.query;
+    return service.getPostsByParam(req, res, option);
+}
+
+exports.updatePost = function(req, res){
+    id = req.params.id;
+    update = req.body;
+    return service.updatePost(req, res, id, update);    
+}
