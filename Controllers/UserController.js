74ff61fceb6bd2e31585878aec455b2c80cbac41
je-ni <jenniferolibie@gmail.com>
2018-06-27 15:35:29 +0100

    added getUsers controller

---
+++ Controllers/UserController.js
@@ -0,0 +1,21 @@
+var service = require('../Services/UserService');
+
+exports.addUser = function(req, res){
+    
+}
+
+exports.getUsers = function(req, res){
+    return service.getAllUsers(req, res);
+}
+
+exports.deleteUser = function(req, res){
+    
+}
+
+exports.getUserByParam = function(req, res){
+    
+}
+
+exports.updateUser = function(req, res){
+    
+}
