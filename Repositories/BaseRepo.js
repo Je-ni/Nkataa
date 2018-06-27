f0ae2db485690463988a3bb9639e4768f807c48a
je-ni <jenniferolibie@gmail.com>
2018-06-27 15:03:20 +0100

    added baserepo file and contents to  repository folder

---
+++ Repositories/BaseRepo.js
@@ -0,0 +1,29 @@
+//creating a constructor for base repos
+function BaseRepo(model){
+    if(!model) throw new Error("A model must be provided");
+    this.model = model;
+}
+
+BaseRepo.prototype.get = function(callback){
+    this.model.find(callback);
+}
+
+BaseRepo.prototype.add = function(data, callback){
+    this.model.create(data, callback);
+}
+
+BaseRepo.prototype.getById = function(id, callback){
+    this.model.findById(id, callback);
+}
+
+BaseRepo.prototype.getByParam = function(option, callback){
+    model.find(option, callback);
+}
+
+BaseRepo.prototype.delete = function(options, callback){
+    this.model.remove(options, callback);
+}
+
+BaseRepo.prototype.update = function(id, options, callback){
+    this.model.findByIdAndUpdate(id, options, callback);
+}
