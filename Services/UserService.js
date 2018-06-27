2328644439bae15cf9e4cb0e7d6612f16983afbd
je-ni <jenniferolibie@gmail.com>
2018-06-27 16:36:49 +0100

    added the service for addUser

--- Services/UserService.js
+++ Services/UserService.js
@@ -1,5 +1,17 @@
 var repo = require('../Repositories/UserRepo');
 
+exports.addUser = function(req, res){
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
+    repo.add(data, function(err){
+        if(err) res.json({err: err, message: 'the user could not be created'});
+        res.json({message: 'the user was created successfully'});
+    });
+}
+
 exports.getAllUsers = function(req, res){
     repo.get({}, '-password', function(err, users){
         if(err) res.json({err: err, message: 'Something went wrong'});
