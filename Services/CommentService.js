18e4f418c664a77484db42e754cc83dd1e13e56b
je-ni <jenniferolibie@gmail.com>
2018-06-28 08:22:15 +0100

    modified the getComments populate method

--- Services/CommentService.js
+++ Services/CommentService.js
@@ -8,14 +8,14 @@ exports.addComment = function(req, res, comment){
 }
 
 exports.getAllComments = function(req, res){
-    repo.get({}, '','user', 'post', function(err, comments){
+    repo.get({}, '',{path: 'user', select: 'name'}, 'post', function(err, comments){
         if(err) res.json({err: err, message: 'Something went wrong'});
         res.json(comments);
     });
 }
 
 exports.getCommentsInPost = function(req, res, options){
-    repo.get(options, '', 'user', 'post',function(err, posts){
+    repo.get(options, '', {path: 'user', select: 'name'}, 'post',function(err, posts){
         if(err) res.json({err: err, message: 'Something went wrong'});
         res.json(posts);
     });
