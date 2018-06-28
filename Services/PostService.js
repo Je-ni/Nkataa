775558ac23c9da49dc7c6c21458464b88f9dbead
je-ni <jenniferolibie@gmail.com>
2018-06-28 14:11:12 +0100

    modified the populate path post comments

--- Services/PostService.js
+++ Services/PostService.js
@@ -13,7 +13,7 @@ exports.addPost = function(req, res, data){
 }
 
 exports.getAllPosts = function(req, res){
-    repo.get({}, '', {path: 'user', select: 'name'}, 'comments', function(err, posts){
+    repo.get({}, '', {path: 'user', select: 'name'}, {path: 'comments', select: '-post'}, function(err, posts){
         if(err) res.json({err: err, message: 'Something went wrong'});
         res.json(posts);
     });
